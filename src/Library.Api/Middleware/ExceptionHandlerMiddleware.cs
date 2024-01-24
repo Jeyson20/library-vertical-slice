20e5fc0e194350894361d10528bb612b523cf7cb
Jeyson Almonte <jjalmonte@humano.com.do>
2024-01-24 00:26:37 -0400

    feat: add exceptionHandlerMiddleware and extensions

---
+++ src/Library.Api/Middleware/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,22 @@
+﻿namespace Library.Api.Middleware;
+
+public class ExceptionHandlerMiddleware(RequestDelegate next)
+{
+	private readonly RequestDelegate _next = next;
+
+	public async Task Invoke(HttpContext ctx)
+	{
+		try
+		{
+			await _next(ctx);
+		}
+		catch (Shared.Exceptions.ValidationException ex)
+		{
+			await Results.UnprocessableEntity(new ValidationResult(ex.Errors)).ExecuteAsync(ctx);
+		}
+		catch (Exception ex)
+		{
+			await Results.Problem(Errors.ServerError(ex.Message)).ExecuteAsync(ctx);
+		}
+	}
+}
---
+++ src/Library.Api/Shared/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Library.Api.Middleware;
+
+namespace Library.Api.Shared.Extensions;
+
+public static class MiddlewareExtensions
+{
+	public static IApplicationBuilder UseMiddleware(this IApplicationBuilder builder)
+	{
+		return builder.UseMiddleware<ExceptionHandlerMiddleware>();
+	}
+}
