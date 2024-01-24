7210759f5be57df528af2471ff9d8cc3b46f63e6
Jeyson Almonte <jjalmonte@humano.com.do>
2024-01-24 00:21:16 -0400

    feat: implement database connection

---
+++ src/Library.Api/Database/SqlConnectionFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.Data;
+
+namespace Library.Api.Database;
+
+internal class SqlConnectionFactory(string connectionString) : IDatabaseConnectionFactory
+{
+	private readonly string _connectionString = connectionString;
+
+	public IDbConnection Connection => new SqlConnection(_connectionString);
+}
---
+++ src/Library.Api/Shared/Interfaces/IDatabaseConnectionFactory.cs
@@ -0,0 +1,9 @@
+﻿using System.Data;
+
+namespace Library.Api.Shared.Interfaces
+{
+	public interface IDatabaseConnectionFactory
+	{
+		IDbConnection Connection { get; }
+	}
+}
