42642022b55e0daf2bee6c7d04a2120b98a9156f
Jeyson Almonte <jjalmonte@humano.com.do>
2024-01-24 00:22:18 -0400

    feat: add validation behavior and validationexception

---
+++ src/Library.Api/Shared/Exceptions/ValidationException.cs
@@ -0,0 +1,13 @@
+﻿
+using FluentValidation.Results;
+
+namespace Library.Api.Shared.Exceptions;
+
+public sealed class ValidationException(IEnumerable<ValidationFailure> failures)
+: Exception("One or more validation failures have ocurred.")
+{
+	public IReadOnlyCollection<Error> Errors { get; } = failures
+		.Distinct()
+		.Select(group => new Error(group.ErrorCode, group.ErrorMessage))
+		.ToArray();
+}
