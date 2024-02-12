da89cba82d157b6e3d081a2a7eac7f00860d0ca5
Jeyson Almonte <jjalmonte@humano.com.do>
2024-02-12 01:25:05 -0400

    feat: add AuthorRepository extension of Repository

---
+++ src/Library.Api/Features/Authors/AuthorRepository.cs
@@ -0,0 +1,15 @@
+﻿using Library.Api.Data;
+
+namespace Library.Api.Features.Authors;
+
+public interface IAuthorRepository : IRepository<Author>;
+
+internal class AuthorRepository(
+	IDbConnectionFactory dbConnectionFactory,
+	ILogger<Repository<Author>> logger) :  Repository<Author>(dbConnectionFactory, logger), IAuthorRepository
+{
+	private readonly IDbConnectionFactory _connectionFactory = dbConnectionFactory;
+	private readonly ILogger<Repository<Author>> _logger = logger;
+	
+	
+}
