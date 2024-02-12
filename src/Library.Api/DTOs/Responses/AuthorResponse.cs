883e822688564e088c7ef18484eec04f6f4f9fa5
Jeyson Almonte <jeysom@live.com>
2024-02-11 20:37:23 -0400

    feat: implement GetAuthorById

---
+++ src/Library.Api/DTOs/Responses/AuthorResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Library.Api.DTOs.Responses
+{
+	public record AuthorResponse
+	{
+		public string Id { get; set; } = string.Empty;
+		public string FirstName { get; set; } = string.Empty;
+		public string LastName { get; set; } = string.Empty;
+		public DateTime DateOfBirth { get; set; }
+	}
+}
---
+++ src/Library.Api/Features/Authors/GetAuthorById.cs
@@ -0,0 +1,52 @@
+﻿using Library.Api.DTOs.Responses;
+
+namespace Library.Api.Features.Authors
+{
+	public static class GetAuthorById
+	{
+		public record Query(string AuthorId) : IRequest<Result<AuthorResponse>>;
+
+		internal sealed class Handler(IDatabaseService databaseService) : IRequestHandler<Query, Result<AuthorResponse>>
+		{
+			private readonly IDatabaseService _databaseService = databaseService;
+			public async Task<Result<AuthorResponse>> Handle(Query request, CancellationToken cancellationToken)
+			{
+				try
+				{
+					var author = await _databaseService
+						.GetById<Author>(request.AuthorId);
+
+					if (author is null)
+					{
+						return Result<AuthorResponse>.NotFound();
+					}
+
+					var authorResponse = author.Adapt<AuthorResponse>();
+
+					return Result.Success(authorResponse);
+				}
+				catch (SqlException error)
+				{
+					return Result<AuthorResponse>.Failure(
+						Errors.DatabaseError(nameof(Author), error.Message));
+				}
+			}
+		}
+	}
+
+	public class GetAuthorByIdEndpoint : ICarterModule
+	{
+		public void AddRoutes(IEndpointRouteBuilder app)
+		{
+			app.MapGet("api/authors/{authorId}", async (string authorId, ISender sender) =>
+			{
+
+				var result = await sender.Send(
+					new GetAuthorById.Query(authorId));
+
+				return result.HandleResult();
+
+			}).WithTags("Authors");
+		}
+	}
+}
