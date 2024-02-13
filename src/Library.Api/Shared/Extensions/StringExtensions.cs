30c9b03be906049d7f8b6f6d3bc32593e140945a
Jeyson Almonte <jjalmonte@humano.com.do>
2024-02-13 01:23:25 -0400

    feat: add StringExtensions and Paginated response

---
+++ src/Library.Api/Shared/Extensions/StringExtensions.cs
@@ -0,0 +1,6 @@
+﻿namespace Library.Api.Shared.Extensions;
+
+public static class StringExtensions
+{
+	public static bool HasValue(this string? param) => !string.IsNullOrEmpty(param) && !string.IsNullOrWhiteSpace(param);
+}
---
+++ src/Library.Api/Shared/Models/Paginated.cs
@@ -0,0 +1,12 @@
+﻿namespace Library.Api.Shared.Models;
+
+public class Paginated<TData>(ICollection<TData> items, int totalCount, int pageNumber, int pageSize)
+{
+	public ICollection<TData> Items { get; } = items;
+	public int PageNumber { get; } = pageNumber;
+	public int PageSize { get; } = pageSize;
+	public int TotalPages { get; } = (int)Math.Ceiling(totalCount / (double)pageSize); 
+	public int TotalCount { get; } = totalCount;
+	public bool HasPreviousPage => PageNumber > 1;
+	public bool HasNextPage => PageNumber < TotalPages;
+}
