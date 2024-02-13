8b49f9ef94f10ae17dec35fa039aabb1738f29a9
Jeyson Almonte <jjalmonte@humano.com.do>
2024-02-13 01:24:15 -0400

    feat: add GetAllAuthors with paginated response

--- src/Library.Api/Features/Authors/AuthorRepository.cs
+++ src/Library.Api/Features/Authors/AuthorRepository.cs
@@ -2,14 +2,52 @@
 
 namespace Library.Api.Features.Authors;
 
-public interface IAuthorRepository : IRepository<Author>;
-
+public interface IAuthorRepository : IRepository<Author>
+{
+	Task<(IEnumerable<Author> Authors,
+		int TotalCount,
+		int PageSize,
+		int PageNumber)> GetAllAuthors(string? firstName, int? pageSize, int? pageNumber);
+}
 internal class AuthorRepository(
 	IDbConnectionFactory dbConnectionFactory,
-	ILogger<Repository<Author>> logger) :  Repository<Author>(dbConnectionFactory, logger), IAuthorRepository
+	ILogger<Repository<Author>> logger) : Repository<Author>(dbConnectionFactory, logger), IAuthorRepository
 {
 	private readonly IDbConnectionFactory _connectionFactory = dbConnectionFactory;
 	private readonly ILogger<Repository<Author>> _logger = logger;
-	
-	
+
+	public async Task<(
+		IEnumerable<Author> Authors,
+		int TotalCount,
+		int PageSize,
+		int PageNumber)> GetAllAuthors(string? firstName, int? pageSize, int? pageNumber)
+	{
+		pageNumber ??= 1;
+		pageSize ??= 20;
+		int offSet = (int)((pageNumber - 1) * pageSize);
+		
+		object? parameters = firstName.HasValue()
+			? new { firstName }
+			: default;
+		
+		string condition = parameters != null ? BuildSearchCondition(parameters) : string.Empty;
+		
+		string query = $"""
+		                SELECT (SELECT COUNT(*)
+		                        FROM Author {condition}) As TotalCount;
+		                SELECT Id, FirstName, LastName, DateOfBirth
+		                FROM Author {condition}
+		                ORDER BY Id ASC
+		                OFFSET {offSet} ROWS FETCH NEXT {pageSize} ROWS ONLY;
+		                """;
+		_logger.LogInformation("Executing: {Query}", query);
+
+		var result = await _connectionFactory.Connection
+			.QueryMultipleAsync(query);
+
+		int totalCount = await result.ReadFirstAsync<int>();
+		var authors = await result.ReadAsync<Author>();
+
+		return (authors, totalCount, (int)pageSize, (int)pageNumber);
+	}
 }
---
+++ src/Library.Api/Features/Authors/GetAllAuthors.cs
@@ -0,0 +1,54 @@
+﻿using Library.Api.DTOs.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Api.Features.Authors;
+
+public static class GetAllAuthors
+{
+	public record Query(
+		string? FirstName,
+		int? PageNumber,
+		int? PageSize) : IRequest<Paginated<AuthorResponse>>;
+
+	internal sealed class Handler(IAuthorRepository repository) : IRequestHandler<Query, Paginated<AuthorResponse>>
+	{
+		private readonly IAuthorRepository _repository = repository;
+
+		public async Task<Paginated<AuthorResponse>> Handle(Query request, CancellationToken cancellationToken)
+		{
+			(var authors, int totalCount, int pageSize, int pageNumber) = await _repository.GetAllAuthors(
+				request.FirstName,
+				request.PageSize,
+				request.PageNumber
+			);
+
+			var authorResponse = authors.Adapt<ICollection<AuthorResponse>>();
+
+			return new Paginated<AuthorResponse>(authorResponse, totalCount, pageNumber, pageSize);
+		}
+	}
+}
+public class GetAllAuthorsEndpoint : ICarterModule
+{
+	public void AddRoutes(IEndpointRouteBuilder app)
+	{
+		app.MapGet("api/authors", async (
+				ISender sender,
+				string? firstName,
+				int? pageNumber,
+				int? pageSize) =>
+			{
+				var result = await sender.Send(
+					new GetAllAuthors.Query(
+						firstName,
+						pageNumber,
+						pageSize));
+
+				return Results.Ok(result);
+
+			}).Produces<Paginated<AuthorResponse>>()
+			.WithTags("Authors")
+			.WithSummary("Get all authors.")
+			.WithOpenApi();
+	}
+}
