f09d0ab85203a22535fc22c411acc5d41fbf01af
Jeyson Almonte <jjalmonte@humano.com.do>
2024-02-13 01:22:56 -0400

    refact: base Errors

--- src/Library.Api/Shared/Models/Errors.cs
+++ src/Library.Api/Shared/Models/Errors.cs
@@ -3,6 +3,6 @@
 public static class Errors
 {
 	internal static Error ServerError(string message) => new("API.ServerError", message);
-	internal static Error DatabaseError(string name, string message) => new($"{name}.SqlError", message);
+	internal static Error DatabaseError(string message) => new($"API.SqlError", message);
 	internal static Error ValidationError(Type type, string message) => new($"{type.Name}.Validation", message);
 }
