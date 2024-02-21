4ccf073a3406d75c412778e07c3b761e26268efa
Jeyson Almonte <jjalmonte@humano.com.do>
2024-02-21 00:34:45 -0400

    feat: add PublisherRepository with Create  and GetAll methods

---
+++ src/Library.Api/Features/Publishers/PublisherRepository.cs
@@ -0,0 +1,33 @@
+﻿using Library.Api.Data;
+
+namespace Library.Api.Features.Publishers;
+
+public interface IPublisherRepository
+{
+	Task<Publisher> Create(Publisher publisher, CancellationToken cancellationToken = default);
+	Task<IEnumerable<Publisher>> GetAll(CancellationToken cancellationToken = default);
+}
+public class PublisherRepository(
+	IDbConnectionFactory dbConnection,
+	ILogger<PublisherRepository> logger) : IPublisherRepository
+{
+	private readonly IDbConnectionFactory _dbConnection = dbConnection;
+	private readonly ILogger<PublisherRepository> _logger = logger;
+	public async Task<Publisher> Create(Publisher publisher, CancellationToken cancellationToken = default)
+	{
+		const string sql = """
+		                   INSERT INTO Publishers (Id, Name)
+		                   OUTPUT INSERTED.*
+		                   VALUES (@Id, @Name)
+		                   """;
+		return await _dbConnection.Connection.QuerySingleAsync<Publisher>(sql, publisher);
+	}
+	public async Task<IEnumerable<Publisher>> GetAll(CancellationToken cancellationToken = default)
+	{
+		const string sql = """
+		                   SELECT Id, Name
+		                   FROM Publishers
+		                   """;
+		return await _dbConnection.Connection.QueryAsync<Publisher>(sql, cancellationToken);
+	}
+}
